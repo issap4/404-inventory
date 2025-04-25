24c266d87be8cb58b3dd4989bc28aab38b00588a
ipa <94088563+issap4@users.noreply.github.com>
2025-04-25 21:13:53 +0200

    Update ping_app.py

--- ping_app.py
+++ ping_app.py
@@ -4,20 +4,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
+import chromedriver_autoinstaller
 
 URL = "https://404-inventory.streamlit.app/"
 
 def ping_streamlit():
+    # Instala automáticamente el chromedriver compatible
+    chromedriver_autoinstaller.install()
+
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920x1080")
 
     driver = webdriver.Chrome(options=options)
     driver.get(URL)
 
     try:
-        # Espera a que aparezca el título de la app como prueba de carga completa
         WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.TAG_NAME, "h1"))
         )
